0834357ea47fe778eb4c3088e03766a5da785537
pradeep <pradeepchandra3196@gmail.com>
2023-11-27 20:48:43 +0530

    first commit

---
+++ src/test/java/com/practice/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.practice.jenkin.api;
+
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	
+	@Test
+	void contextLoads() {
+		logger.info("Application started...");
+		Assertions.assertTrue(true);
+	}
+
+}
